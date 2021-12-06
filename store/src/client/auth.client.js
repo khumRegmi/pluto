39b0725462ed291f3404682d0eed25a9bb8994e4
Khum Regmi <kpr.regmi088@gmail.com>
2021-12-06 13:26:54 -0500

    Update auth.client.js

--- store/src/client/auth.client.js
+++ store/src/client/auth.client.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  process.env.NODE_ENV !== "production"
+  process.env.NODE_ENV !== "production" //env is not production (true)
     ? process.env.REACT_APP_DEV_API_URL
     : process.env.REACT_APP_PROD_API_URL;
 
